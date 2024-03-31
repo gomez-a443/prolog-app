0676743c5a1f5faa31cd254714544aa80c2126a0
Gomez-a443 <aparnaraj443@gmail.com>
2024-03-31 15:07:32 +0530

    Completed Task-1 Header

---
+++ config/routes.tsx
@@ -0,0 +1,11 @@
+export const Routes = {
+  home: "/",
+  projects: "/dashboard/",
+  issues: "/dashboard/issues",
+  alerts: "/dashboard/alerts",
+  users: "/dashboard/users",
+  settings: "/dashboard/settings",
+  products: "/products",
+  documentation: "/documentaion",
+  pricing: "/pricing",
+};
---
+++ pages/index.jsx
@@ -0,0 +1,35 @@
+import { Routes } from "@config/routes";
+import styles from "./index.module.scss";
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        <div className={styles.heading}>
+          <a href={Routes.home}>Home</a>
+          <a href={Routes.products}>Products</a>
+          <a href={Routes.documentation}>Documentation</a>
+          <a href={Routes.pricing}>Pricing</a>
+        </div>
+        <a href={Routes.projects} className={styles.dashboard}>
+          Open Dashboard
+        </a>
+      </header>
+      <button
+        className={styles.contactButton}
+        onClick={() =>
+          alert(
+            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
+          )
+        }
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/message.svg" alt="Contact" />
+      </button>
+    </div>
+  );
+};
+
+export default IssuesPage;
